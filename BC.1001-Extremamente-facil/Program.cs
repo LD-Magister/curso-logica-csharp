50c14b3affac7c6ffefe4846481b11dbb4955783
Alyder <alyderalves@gmail.com>
2022-06-19 19:08:03 -0300

    Resolução dos Exercícios

---
+++ BC.1001-Extremamente-facil/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd
+{
+    class iniciante
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int x = (A + B);
+
+            Console.WriteLine("X = " + x);
+        }
+    }
+}
